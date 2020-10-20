18609d3bce1ee193d5d952a08c5bee2184ff86be
Daniel Giribet <dani@calidos.cat>
2020-10-21 00:26:28 +0200

    secret management for minio cleanup

--- argo/minio/cleanup.sh
+++ argo/minio/cleanup.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+MINIO_SERVICE_URL="$1"
+mc alias set minio-local "$MINIO_SERVICE_URL" ${ACCESS_KEY} ${SECRET_KEY}
 # WARNING this nukes the content of all the bucket WARNING
 # WARNING this nukes the content of all the bucket WARNING
 # WARNING this nukes the content of all the bucket WARNING
 # WARNING this nukes the content of all the bucket WARNING
 for i in $(mc ls minio-local/my-bucket/ | awk -e '{print $5}' | grep workflow); do
-	mc rm --recursive --force minio-local/my-bucket/$i
+	mc rm --recursive --force minio-local/my-bucket/"$i"
 done
