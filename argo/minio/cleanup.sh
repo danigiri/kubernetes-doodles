6d419cdbf5429ed057e8f9da63364b25de9b6a90
Daniel Giribet <dani@calidos.cat>
2020-10-21 00:08:23 +0200

    prepare for non workflow storage

--- argo/minio/cleanup.sh
+++ argo/minio/cleanup.sh
@@ -4,6 +4,6 @@
 # WARNING this nukes the content of all the bucket WARNING
 # WARNING this nukes the content of all the bucket WARNING
 # WARNING this nukes the content of all the bucket WARNING
-for i in $(mc ls minio-local/my-bucket/ | awk -e '{print $5}'); do
+for i in $(mc ls minio-local/my-bucket/ | awk -e '{print $5}' | grep workflow); do
 	mc rm --recursive --force minio-local/my-bucket/$i
 done
